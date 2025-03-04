57c7ea27d58e21580bf9ada5679f89f4562955f2
hafsaYaman <hafsa7yaman@gmail.com>
2025-03-04 01:43:52 +0000

    update

--- main.py
+++ main.py
@@ -15,3 +15,36 @@ restaurant_tables = [
     [6, 'x', 'o', 'o', 'o', 'o', 'o']
 ]
 
+def find_table_party(tables, timeslot, party_size):
+    for table in range(1, len(tables[0])):
+        capacity = int(tables[0][table].split('(')[1].split(')')[0])
+        if table[timeslot][table]=='o' and capacity >=party_size:
+            return tables[0][table]
+    return None
+
+
+def all_for_party(tables, timeslot, party_size):
+    suitable = []
+    for index in range(1, len(tables[0])):
+        capacity = int(tables[0][index].split('(')[1].split(')')[0])
+        if restaurant_tables[timeslot][index] == 'o' and capacity >= party_size:
+            suitable.append(tables[0][index])
+    return suitable
+
+
+def combined_party_tables(tables, timeslot, party_size):
+    combinations = []
+    for index in range(1, len(tables[0])-1):
+        capacity = int(tables[0][index].split('(')[1].split(')')[0])
+        next_capacity = int(tables[0][index + 1].split('(')[1].split(')')[0])
+        if tables[timeslot][index] == 'o' and capacity >= party_size:
+            combinations.append([tables[0][index]])
+        if tables[timeslot][index] == 'o' and restaurant_tables[timeslot][index + 1] == 'o' and (capacity + next_capacity) >= party_size:
+            combinations.append([tables[0][index], tables[0][index + 1]])
+    return combinations
+
+
+
+
+size = 4
+print(combined_party_tables(restaurant_tables, 1, size))
